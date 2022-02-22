72707a8712d83eeb1d47c0d11c6644ebb7577912
Xumoyun <rabbimovxumoyun22@gmail.com>
2022-02-22 21:39:31 +0500

    addCart saqlanishlari bajarildi

--- src/redux/type/types.js
+++ src/redux/type/types.js
@@ -3,3 +3,5 @@
 export const navButton = "navButtonClick"
 export const updateState = "updateState"
 export const addcart = "AddCart"
+export const deleteCt = "DeleteAddCart"
+export const summa = "SummCart"
